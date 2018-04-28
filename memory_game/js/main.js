c75f4c482d31369c1ac36a0534835eaad194f46b
aderius sumlin <aderiussumlin@gmail.com>
2018-04-28 15:53:39 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,22 @@
-var cardOne='king';
-var cardTwo='queen';
-var cardThree='king';
-var cardFour='queen';
+
 
 console.log("User flipped " + cardOne);
+var cards=['queen','queen','king','king'];
+var cardInPlay=[];
+var cardOne=cards[0];
+cardInPlay.push(cardOne);
+
+console.log('user flipped ' + cardOne);
+
+var cardTwo=cards[2];
+cardInPlay.push(cardTwo);
+
+console.log('user flipped ' + cardTwo);
+
+if (cardInPlay.length === 2) {
+	if (cardInPlay[0] === cardInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}	
+}
