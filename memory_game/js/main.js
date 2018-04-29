b626f59b3a73ea4767c5914f2950b12e9d152084
aderius sumlin <aderiussumlin@gmail.com>
2018-04-29 16:34:28 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -31,15 +31,24 @@ function checkForMatch(){
 			alert("Sorry, try again.");
 		}
 }
-function flipCard(cardId) {
+function flipCard() {
+	 var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log("User flipped " + cards[cardId].suit);
 	console.log("User flipped " + cards[cardId].cardImage);
 	cardInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
 	if (cardInPlay.length === 2) {
 		checkForMatch();
 	}
 }
-
-flipCard(0);
-flipCard(2);
+function createBoard(){
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click',flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+createBoard();
