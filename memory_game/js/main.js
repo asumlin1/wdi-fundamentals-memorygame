bf47f84ffdaba91b380fdb30f8ffab38c00301e7
aderius sumlin <aderiussumlin@gmail.com>
2018-04-29 14:23:35 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,27 @@
 
 
-var cards=['queen','queen','king','king'];
+var cards=[
+	{
+		rank:"queen",
+		suit:"hearts",
+		cardImage:"images/queen-of-hearts.png"
+	},
+	{
+		rank:"queen",
+		suit:"diamonds",
+		cardImage:"images/queen-of-diamonds.png"
+	},
+	{
+		rank:"king",
+		suit:"hearts",
+		cardImage:"images/king-of-hearts.png"
+	},
+	{
+		rank:"king",
+		suit:"queen-of-diamonds",
+		cardImage:"images/king-of-diamonds.png"
+	}
+];
 var cardInPlay=[];
 	
 function checkForMatch(){
@@ -11,8 +32,10 @@ function checkForMatch(){
 		}
 }
 function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].suit);
+	console.log("User flipped " + cards[cardId].cardImage);
+	cardInPlay.push(cards[cardId].rank);
 	if (cardInPlay.length === 2) {
 		checkForMatch();
 	}
